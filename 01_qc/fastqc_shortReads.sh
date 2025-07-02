3e63e444f5b73e7d19cbede49173571fd3eb3422
ElizabethBrooks <merimaat1@gmail.com>
2025-07-02 11:59:06 -0400

    Added initial analysis scripts

---
+++ 01_qc/fastqc_shortReads.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N fastqc_MultiGenome_jobOutput
+
+# Script to perform fastqc quality control of paired end reads
+# Usage: qsub fastqc_shortReads.sh inputsFile
+# Usage Ex: qsub fastqc_shortReads.sh CON6 raw
+# Usage Ex: qsub fastqc_shortReads.sh CON6 trimmed
+
+# Required modules for ND CRC servers
+module load bio
+
+# retrieve input arguments
+inputGenotype=$1
+inputsType=$2
+
+# Retrieve paired reads absolute path for alignment
+readPath=$(grep "pairedReads:" ../"inputData/shortReads/inputPaths_D_melanica.txt" | tr -d " " | sed "s/pairedReads://g")
+# Retrieve analysis outputs absolute path
+outputsPath=$(grep "outputs:" ../"inputData/shortReads/inputPaths_D_melanica.txt" | tr -d " " | sed "s/outputs://g")
+
+# Make a new directory for project analysis
+projectDir=$(basename $readPath)
+outputsPath=$outputsPath"/"$projectDir
+#mkdir $outputsPath
+
+# check inputs type
+if [[ $inputsType == "trimmed" ]]; then
+	# set the directory for inputs
+	readPath=$outputsPath"/trimmed"
+	# set the directory for analysis
+	qcOut=$outputsPath"/qc_trimmed_"$inputGenotype
+elif [[ $inputsType == "raw" ]]; then
+	# set the directory for analysis
+	qcOut=$outputsPath"/qc_raw_"$inputGenotype
+fi
+
+# Make a new directory for analysis
+mkdir $qcOut
+# Check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $qcOut directory already exsists... please remove before proceeding."
+	exit 1
+fi
+# Move to the new directory
+cd $qcOut
+
+# Name version output file
+versionFile=$qcOut"/software_version_summary.txt"
+
+# Report software version
+fastqc -version > $versionFile
+
+# perform QC
+fastqc $readPath"/"$inputGenotype*\.f*q.gz -o $qcOut
+
+# run multiqc
+multiqc $qcOut -o $qcOut -n "multiqc_raw"
+
+# Print status message
+echo "Analysis complete!"
---
+++ 03_alignment/alignment_hisat2.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#$ -M ebrooks5@nd.edu
+#$ -m abe
+#$ -r n
+#$ -N alignment_hisat2_jobOutput
+#$ -pe smp 4
+
+# Script to perform hisat2 alignment of trimmed
+# paired end reads
+# Note that a hisat2 genome refernce build folder needs to be generated first
+# usage: qsub alignment_hisat2.sh
+
+#Required modules for ND CRC servers
+module load bio/2.0
+#module load bio/hisat2/2.1.0
+
+#Retrieve genome reference absolute path for alignment
+buildFile=$(grep "genomeReference:" ../inputData/shortReads/inputPaths_D_melanica.txt | tr -d " " | sed "s/genomeReference://g")
+# Retrieve analysis outputs absolute path
+outputsPath=$(grep "outputs:" ../"inputData/shortReads/inputPaths_D_melanica.txt" | tr -d " " | sed "s/outputs://g")
+# Retrieve paired reads absolute path for alignment
+readPath=$(grep "pairedReads:" ../"inputData/shortReads/inputPaths_D_melanica.txt" | tr -d " " | sed "s/pairedReads://g")
+
+# Make a new directory for project analysis
+projectDir=$(basename $readPath)
+outputsPath=$outputsPath"/"$projectDir
+
+# set inputs absolute path
+trimmedFolder=$outputsPath"/trimmed"
+
+# move to outputs directory
+cd "$outputsPath"
+
+# set output directory name
+outputFolder=$(echo $trimmedFolder | sed "s/trimmed/aligned/g")
+# create output directory
+mkdir "$outputFolder"
+# check if the folder already exists
+if [ $? -ne 0 ]; then
+	echo "The $outputFolder directory already exsists... please remove before proceeding."
+	exit 1
+fi
+
+#Name output file of inputs
+inputOutFile=$outputFolder"/software_summary.txt"
+#Add software version to output summary file
+hisat2 --version > $inputOutFile
+
+#Build output directory for Hisat reference
+buildOut="$outputsPath"/"reference_hisat2_build"
+#Trim .fa file extension from build file
+buildFileNoPath=$(basename $buildFile)
+buildFileNoEx=$(echo $buildFileNoPath | sed 's/\.fasta//' | sed 's/\.fna//' | sed 's/\.fa//')
+
+#Loop through all forward and reverse paired reads and run Hisat2 on each pair
+# using 8 threads and samtools to convert output sam files to bam
+for f1 in "$inputsPath"/"$trimmedFolder"/*pForward.fq.gz; do
+	#Trim extension from current file name
+	curSample=$(echo $f1 | sed 's/.pForward\.fq\.gz//')
+	#Trim file path from current file name
+	curSampleNoPath=$(basename $f1)
+	curSampleNoPath=$(echo $curSampleNoPath | sed 's/.pForward\.fq\.gz//')
+	#Create directory for current sample outputs
+	mkdir "$outputFolder"/"$curSampleNoPath"
+	#Run hisat2 with default settings
+	echo "Sample $curSampleNoPath is being aligned and converted..."
+	hisat2 -p 4 -q -x "$buildOut"/"$buildFileNoEx" -1 "$f1" -2 "$curSample"_pReverse.fq.gz -S "$outputFolder"/"$curSampleNoPath"/accepted_hits.sam --summary-file "$outputFolder"/"$curSampleNoPath"/alignedSummary.txt
+	#Convert output sam files to bam format for downstream analysis
+	samtools view -@ 4 -bS "$outputFolder"/"$curSampleNoPath"/accepted_hits.sam > "$outputFolder"/"$curSampleNoPath"/accepted_hits.bam
+	#Remove the now converted .sam file
+	rm "$outputFolder"/"$curSampleNoPath"/accepted_hits.sam
+	# status message
+	echo "Sample $curSampleNoPath has been aligned and converted!"
+done
