0e73173bf32a03ad11bc8f50bd1ec7b6b281d604
ElizabethBrooks <merimaat1@gmail.com>
2025-08-13 22:58:09 -0400

    Updating quantification scripts

--- 05_quantification/generateMergedTable.sh
+++ 05_quantification/generateMergedTable.sh
@@ -11,11 +11,11 @@ echo "gene" > $inputsPath"/counts_merged.csv"
 cat $inputsPath"/MUV10_S10_L004/counts.txt" | cut -f1 >> $inputsPath"/counts_merged.csv"
 
 # merge counts for each sample
-for i in $inputsPath"/"*; do 
+for i in $inputsPath"/"*"/"; do 
 	newName=$(basename $i | sed "s/_S.*_L004//g")
 	echo $newName > $inputsPath"/counts_merged.tmp.csv"
 	cat $i/counts.txt | cut -f2 >> $inputsPath"/counts_merged.tmp.csv"
-	paste -d, $inputsPath"/counts_merged.csv" $inputsPath"/counts_merged.tmp.csv"
+	paste -d, $inputsPath"/counts_merged.csv" $inputsPath"/counts_merged.tmp.csv" >> $inputsPath"/counts_merged.tmp.csv"
 done
 
 # clean up
